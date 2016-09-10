6b253ee46d0b0ce96e683ed22e54afc8e62ee4a9
Michael Feathers <michael.feathers@gmail.com>
2016-09-10 18:04:49 -0400

    added test data for dup marker case

--- spec/data/a.rb
+++
@@ -1,8 +0,0 @@
-
-
-def scythe_probe name
-end
-
-if 1 < 2
-  scythe_probe("marker0")
-end
---
+++ spec/data/duplicate_markers.rb
@@ -0,0 +1,13 @@
+
+
+def scythe_probe name
+end
+
+if 1 < 2
+  scythe_probe("marker0")
+else
+  scythe_probe("marker0")
+  scythe_probe("marker0")
+  scythe_probe("marker1")
+  scythe_probe("marker1")
+end
