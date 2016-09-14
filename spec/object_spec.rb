98999da894109f3385020df3447f7db7c5a3e645
Michael Feathers <michael.feathers@gmail.com>
2016-09-14 08:02:59 -0400

    changed spec to accomodate name change of test data

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -5,7 +5,6 @@ require 'functions'
 describe Object do
 
   it 'returns filenames from a valid file spec' do
-    expect(file_names("spec/data").grep(/a\.rb$/).count).to eq(1)
     expect(file_names("spec/data").grep(/b\.rb$/).count).to eq(1)
   end
 
