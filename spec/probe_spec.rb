6353a4afe9ea7c4e780c1fda6638cb35c1e1f8f1
Michael Feathers <michael.feathers@gmail.com>
2016-11-03 11:09:13 -0400

    added tests for silent?

--- spec/probe_spec.rb
+++ spec/probe_spec.rb
@@ -25,4 +25,35 @@ describe Probe do
     expect(probe.days_silent?(check_time)).to eq(0)
   end
 
+  it "calculates seconds silent" do
+    day = DateTime.parse('2001-01-01').to_time.to_i
+    next_day = DateTime.parse('2001-01-02').to_time.to_i
+    secs_per_day = 86400
+
+    probe = Probe.new("example", day) 
+    expect(probe.seconds_silent?(next_day)).to eq(secs_per_day)
+  end
+
+  
+  it "calculates seconds silent through silent? method" do
+    day = DateTime.parse('2001-01-01').to_time.to_i
+    next_day = DateTime.parse('2001-01-02').to_time.to_i
+    secs_per_day = 86400
+
+    probe = Probe.new("example", day) 
+    expect(probe.silent?(next_day, :seconds)).to eq(secs_per_day)
+  end
+
+    
+  it "calculates days silent through silent? method" do
+    day = DateTime.parse('2001-01-01').to_time.to_i
+    next_day = DateTime.parse('2001-01-02').to_time.to_i
+
+    probe = Probe.new("example", day) 
+    expect(probe.silent?(next_day, :days)).to eq(1)
+  end
+
+
+
+
 end
