5282269a97b7d03319af13e7ca0ae59f40eb3044
Michael Feathers <michael.feathers@gmail.com>
2016-11-08 06:58:07 -0400

    renamed probe argument

--- probes/ruby_probe.rb
+++ probes/ruby_probe.rb
@@ -2,11 +2,11 @@
 
 require 'fileutils'
 
-def scythe_probe name
+def scythe_probe marker
   dir = File.expand_path(ENV["SCYTHE_PROBE_DIR"])
   return unless dir && File.directory?(dir)
 
-  fn = File.join(dir, name + ".scythe_probe")
+  fn = File.join(dir, marker + ".scythe_probe")
   return unless File.exist?(fn)
 
   FileUtils.touch(fn)
