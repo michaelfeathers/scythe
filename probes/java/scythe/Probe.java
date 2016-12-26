48b791292848525c4b8f94ed65316a848955309d
Michael Feathers <michael.feathers@gmail.com>
2016-12-26 15:44:54 -0500

    added java support and created dirs for probe langs

---
+++ probes/java/scythe/Probe.java
@@ -0,0 +1,27 @@
+package scythe;
+
+import java.io.File;
+
+public class Probe {
+	
+	public static void scytheProbe(String marker) {
+		try {
+		 String path = System.getenv("SCYTHE_PROBE_DIR");
+		 File dir = new File(path);
+		 
+		 if (!dir.exists() || !dir.isDirectory())
+			 return;
+		 
+		 File file = new File(dir.getPath(), marker + ".scythe_probe");
+		 
+		 if (file.exists())
+			 return;
+		 
+		 file.setLastModified(System.currentTimeMillis());
+		 
+		} catch(Exception e) {
+		
+		}
+		 
+	}
+}
---
+++ probes/java/scythe/ProbeTest.java
@@ -0,0 +1,49 @@
+package scythe;
+
+import static org.junit.Assert.*;
+import static scythe.Probe.*;
+
+import java.io.File;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ProbeTest {
+
+	private final String EXISTING_MARKER = "java_existing";
+
+	private String path(String marker) {
+		 String path = System.getenv("SCYTHE_PROBE_DIR");
+		 File file = new File(path, marker + ".scythe_probe");
+		 return file.getAbsolutePath();
+	}
+	
+	@Test
+	public void environmentSet() {
+		assertNotNull(System.getenv("SCYTHE_PROBE_DIR"));
+	}
+	
+	@Test 
+	public void testingMarkerExists() {
+		assertTrue(new File(path(EXISTING_MARKER)).exists());
+		
+	}
+	
+	@Test
+	public void doesNotTouchOnNonExisting() {
+		String marker = "java_not_existing";
+		scythe_probe(marker);
+		
+		assertFalse(new File(path(marker)).exists());
+	}
+
+	@Test
+	public void touchesOnExisting() {
+		scythe_probe(EXISTING_MARKER);
+		
+		long markerTime = new File(path(EXISTING_MARKER)).lastModified();
+	
+		assertTrue(System.currentTimeMillis() - markerTime > 10);	
+	}
+
+}
